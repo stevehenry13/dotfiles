3d2dac8f37746422756dad3b4807395e773334de
Steve Henry <stevehenry13@gmail.com>
2013-12-27 11:57:20 -0500

    Fix install script bugs

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 for file in $(ls $(dirname $0) | grep -v *.pub | grep -v $(basename $0)); do
-  if [ -f ~/.$file ]; then
+  if [ -e ~/.$file ]; then
     mv ~/.$file{,.bak}
   fi
 
-  ln -s $file ~/.$file
+  ln -s $(pwd)/$file ~/.$file
 done
 
 # ssh pub keys should be add to authorized_keys, so that passwordless ssh works
-mkdir -p .ssh
-cat $(dirname $0)/*.pub >> .ssh/authorized_keys
+for file in $(ls $(dirname $0)/*.pub); do
+  if [ -z "$(grep "$(cat $file)" ~/.ssh/authorized_keys 2>/dev/null)" ]; then
+    mkdir -p ~/.ssh
+    cat $file >> ~/.ssh/authorized_keys
+  fi
+done
