69a2b760a868a285f3ba090a8fe85c8c986ff9d8
Steve Henry <stevehenry13@gmail.com>
2015-12-18 22:19:17 -0500

    Remove ssh link from install script

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-file_list=$(ls $(dirname $0) | grep -v -e "subversion" -e "ssh" -e "rvm" -e "$(basename $0)")
+file_list=$(ls $(dirname $0) | grep -v -e "subversion" -e "rvm" -e "$(basename $0)")
 
 # only link the files under the directory, not the whole directory (like chef and vim)
 file_list="$file_list subversion/config"
-file_list="$file_list ssh/config"
 file_list="$file_list rvm/gemsets/global.gems"
 
 for file in $file_list; do
@@ -29,17 +28,6 @@ fi
 
 # symlink for X config
 ln -s ~/.Xdefaults ~/.Xresources
-
-# ssh pub keys should be added to authorized_keys, so that passwordless ssh works
-pub_file_list=$(find $(dirname $0)/ssh -name "*.pub")
-for file in $pub_file_list; do
-  if [ -z "$(grep "$(cat $file)" ~/.ssh/authorized_keys 2>/dev/null)" ]; then
-    mkdir -p ~/.ssh
-    cat $file >> ~/.ssh/authorized_keys
-    chmod 700 ~/.ssh
-    chmod 640 ~/.ssh/authorized_keys
-  fi
-done
 
 # Fetch vundle
 mkdir -p vim/bundle
